d95726207af2ba420f0c7dda740f0d832ed41011
Aryannayar <aryan.nayar@procdna.com>
2025-04-27 11:36:12 +0530

    Web socket communication established

---
+++ frontend/react-ws/src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+  const [message, setMessage] = useState('');
+  const [inputMessage,setInputMessage]=useState('');
+
+  useEffect(()=>{
+    const socket=new WebSocket('ws://localhost:8080')
+    socket.onopen=()=>{
+      console.log('Connected to server')
+    
+    }
+    socket.onmessage=(event)=>{
+      console.log('Message from server',event.data)
+      setMessage(event.data)
+    }
+    socket.onclose=()=>{
+      console.log('Connection closed')
+    }
+    socket.onerror=(error)=>{
+      console.error('WebSocket error:',error)
+    }
+    setSocket(socket)
+  },[])
+
+  if(!socket){
+    return <div>
+     Connecting to socket server... 
+    </div>
+  }
+
+  return (
+    <>
+    <input value={inputMessage} onChange={(e)=>setInputMessage(e.target.value)}></input>
+    <button onClick={()=>{
+      socket.send(inputMessage)
+    }}>Send
+    </button>
+    {message}
+    
+    </>
+  )
+}
+
+export default App
